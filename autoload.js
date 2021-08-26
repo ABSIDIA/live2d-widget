542ae08ced7d505dd1f89bbcff2e5560f1b0700b
小蓝莓ABSIDIA <61440206+ABSIDIA@users.noreply.github.com>
2021-08-27 07:08:29 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/ABSIDIA/live2d-widget@1.1.1/";
+const live2d_path = "https://hk.lolicon.team/ABSIDIA/Live2D-API/master/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -34,7 +34,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/ABSIDIA/live2d-api-hanasaki@1.1.0/"
+			cdnPath: "https://hk.lolicon.team/ABSIDIA/live2d-widget/main/"
 		});
 	});
 }
